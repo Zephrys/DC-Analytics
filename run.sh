f03f20f35f047bbb056d9ba4173b2f169465bd9b
shubh24 <shubhankar24@gmail.com>
2016-11-15 15:43:02 +0530

    restart on crash

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
-killall -9 python
-python sheriffbot.py &
+#!/bin/bash
+until killall -9 python; python sheriffbot.py; do
+    echo "'sheriffbot.py' crashed with exit code $?. Restarting..." >&2
+    sleep 1
+done
